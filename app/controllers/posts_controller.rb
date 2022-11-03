f47c242b2bde265e409c70adbe17d8bccd45e019
samorey11 <samorey@outlook.com>
2022-11-02 22:44:55 -0400

    New Post working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def index
     render 'index'
+  end
+
+  def create
+    new_post = Post.create(content: params[:content])
+
+    render json: { post: new_post }
   end
 end
